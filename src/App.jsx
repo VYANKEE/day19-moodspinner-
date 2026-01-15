2dbcc6fe2a35585a369e1abf385e4150a866eca5
Vyankee <vyankateshk1604@gmail.com>
2026-01-15 21:10:50 +0530

    Day 19 Completed : Mood Spinner App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './Landing';
-import SpinnerGame from './SpinnerGame';
+import SpinnerGame from './Spinnergame';
 import './App.css';
 
 function App() {
